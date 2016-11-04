f0720a3a05fca93bfe35d731fbeb7613c7322c1a
Mark Ferguson <maggot.iiss@sky.com>
2016-11-04 09:24:30 +0000

    Summary

--- src/genetics/GeneticsController.java
+++ src/genetics/GeneticsController.java
@@ -1111,6 +1111,7 @@ public class GeneticsController implements Initializable {
       + "republished or distributed without the consent of \n"
       + "the author who contributed it.\n";
     JTextArea jta = new JTextArea(txt, 50, 50);
+    jta.setEditable(false);
     JScrollPane jsp = new JScrollPane(jta);
     jf.getContentPane().add(jsp);
     jf.setSize(500, 500);
